d722dde04fa7f0632fbedfde181234fdb56fe1b6
Tuantrung <tuantrung03121997@gmail.com>
2021-11-23 02:07:36 +0700

    Done exercises of day 1

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -1,21 +1,53 @@
-# @author Tuantrung
-# Introduction
-# Day 1 - 30DaysOfPython Challenge
+# author: Tuan Trung
+# 30 Days Of Python: Day 1 - Introduction
 
-print(2 + 3)   # addition(+)
-print(3 - 1)   # subtraction(-)
-print(2 * 3)   # multiplication(*)
-print(3 / 2)   # division(/)
+# Exercises - Day 1
+import platform
+
+# 1. Check the python version you are using
+print('Current version of Python is {}'.format(platform.python_version()))
+
+"""
+  2. Open the python interactive shell and do the following operations.
+     The operands are 3 and 4. Check the example above.
+"""
+print(2 + 3)  # addition(+)
+print(3 - 1)  # subtraction(-)
+print(2 * 3)  # multiplication(*)
+print(3 / 2)  # division(/)
 print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
+print(3 % 2)  # modulus(%)
 print(3 // 2)  # Floor division operator(//)
 
-# Checking data types
+"""
+  3. Write strings on the python interactive shell. The strings are the following:
+     - Your name
+     - Your family name
+     - Your country
+     - I am enjoying 30 days of python
+"""
+print('My name is: {}'.format('Tuan Trung'))
+print("My family name: {}".format('Nguyen Xuan'))
+print("My country is: {}".format('Vietnam'))
+print("I am enjoying 30 days of python")
 
-print(type(10))                  # Int
-print(type(3.14))                # Float
-print(type(1 + 3j))              # Complex
-print(type('Asabeneh'))          # String
-print(type([1, 2, 3]))           # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Tuple
+"""
+  4. Check the data types of the following data:
+     - 10
+     - 9.8
+     - 3.14
+     - 4 - 4j
+     - ['Asabeneh', 'Python', 'Finland']
+     - Your name
+     - Your family name
+     - Your Country
+"""
+print("Data type of 10 is: {}".format(type(10)))
+print("Data type of 9.8 is: {}".format(type(9.8)))
+print("Data type of 3.14 is: {}".format(type(3.14)))
+print("Data type of ['Asabeneh', 'Python', 'Finland'] is: {}".format(type(['Asabeneh', 'Python', 'Finland'])))
+print("Data type of Your name is: {}".format(type('Your name')))
+print("Data type of Your family name is: {}".format(type('Your family name')))
+print("Data type of Your Country is: {}".format(type('Your Country')))
+
+
