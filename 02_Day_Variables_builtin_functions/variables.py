a01f1f50fb1d5d5e1ceca35d17f72ee6c98dbc2b
Tuantrung <tuantrung03121997@gmail.com>
2021-11-23 02:32:19 +0700

    Done half of exercise 2

--- 02_Day_Variables_builtin_functions/variables.py
+++ 02_Day_Variables_builtin_functions/variables.py
@@ -1,40 +1,75 @@
+# Author: Tuan Trung
+# 30 Days Of Python: Day 2 - Variables, Builtin Functions
 
-# Variables in Python
+# Exercises - Day 2
 
-first_name = 'Asabeneh'
-last_name = 'Yetayeh'
-country = 'Finland'
-city = 'Helsinki'
-age = 250
-is_married = True
-skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
-person_info = {
-    'firstname':'Asabeneh', 
-    'lastname':'Yetayeh', 
-    'country':'Finland',
-    'city':'Helsinki'
-    }
+# Exercises: Level 1
 
-# Printing the values stored in the variables
+# 3. Declare a first name variable and assign a value to it
+first_name = 'Tuan Trung'
 
-print('First name:', first_name)
-print('First name length:', len(first_name))
-print('Last name: ', last_name)
-print('Last name length: ', len(last_name))
-print('Country: ', country)
-print('City: ', city)
-print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills)
-print('Person information: ', person_info)
+# 4. Declare a last name variable and assign a value to it
+last_name = 'Nguyen Xuan'
 
-# Declaring multiple variables in one line
+# 5. Declare a full name variable and assign a value to it
+full_name = first_name + ' ' + last_name
 
-first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+# 6. Declare a country variable and assign a value to it
+country = 'Vietnam'
 
-print(first_name, last_name, country, age, is_married)
-print('First name:', first_name)
-print('Last name: ', last_name)
-print('Country: ', country)
-print('Age: ', age)
-print('Married: ', is_married)
+# 7. Declare a city variable and assign a value to it
+city = 'Hanoi'
+
+# 8. Declare an age variable and assign a value to it
+age = 23
+
+# 9. Declare a year variable and assign a value to it
+year = 2021
+
+# 10. Declare a variable is_married and assign a value to it
+is_married = False
+
+# 11. Declare a variable is_true and assign a value to it
+is_true = True
+
+# 12. Declare a variable is_light_on and assign a value to it
+is_light_on = True
+
+# 13. Declare multiple variable on one line
+a, b, c = 10, '12', 'ok'
+
+# Exercises: Level 2
+
+# 1. Check the data type of all your variables using type() built-in function
+print()
+# skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+# person_info = {
+#     'firstname':'Asabeneh',
+#     'lastname':'Yetayeh',
+#     'country':'Finland',
+#     'city':'Helsinki'
+#     }
+#
+# # Printing the values stored in the variables
+#
+# print('First name:', first_name)
+# print('First name length:', len(first_name))
+# print('Last name: ', last_name)
+# print('Last name length: ', len(last_name))
+# print('Country: ', country)
+# print('City: ', city)
+# print('Age: ', age)
+# print('Married: ', is_married)
+# print('Skills: ', skills)
+# print('Person information: ', person_info)
+#
+# # Declaring multiple variables in one line
+#
+# first_name, last_name, country, age, is_married = 'Asabeneh', 'Yetayeh', 'Helsink', 250, True
+#
+# print(first_name, last_name, country, age, is_married)
+# print('First name:', first_name)
+# print('Last name: ', last_name)
+# print('Country: ', country)
+# print('Age: ', age)
+# print('Married: ', is_married)
