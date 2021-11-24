632dec21ee37cbb1da780286e3f298e12014def3
Tuantrung <tuantrung03121997@gmail.com>
2021-11-24 10:36:10 +0700

    Done half of day 3

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -1,124 +1,84 @@
-# Arithmetic Operations in Python
-# Integers
+# Author: Tuan Trung
+# 30 Days Of Python: Day 3 - Operators
 
-print('Addition: ', 1 + 2)
-print('Subtraction: ', 2 - 1)
-print('Multiplication: ', 2 * 3)
-print ('Division: ', 4 / 2)                         # Division in python gives floating number
-print('Division: ', 6 / 2)
-print('Division: ', 7 / 2)
-print('Division without the remainder: ', 7 // 2)   # gives without the floating number or without the remaining
-print('Modulus: ', 3 % 2)                           # Gives the remainder
-print ('Division without the remainder: ',7 // 3)
-print('Exponential: ', 3 ** 2)                     # it means 3 * 3
+# Exercises - Day 3
+import math
+# 1. Declare your age as integer variable
+age = 23
 
-# Floating numbers
-print('Floating Number,PI', 3.14)
-print('Floating Number, gravity', 9.81)
+# 2. Declare your height as a float variable
+height = 1.72
 
-# Complex numbers
-print('Complex number: ', 1+1j)
-print('Multiplying complex number: ',(1+1j) * (1-1j))
+# 3. Declare a complex number variable
+complex_number = 1+2j
 
-# Declaring the variable at the top first
+"""
+  4. Write a script that prompts the user to enter base and height of the triangle 
+  and calculate an area of this triangle (area = 0.5 x b x h).
+"""
+# base = float(input('Enter base: '))
+# height = float(input('Enter height: '))
+# area = 0.5 * base * height
+# print('The area of triangle is: {}\n'.format(area))
 
-a = 3 # a is a variable name and 3 is an integer data type
-b = 2 # b is a variable name and 3 is an integer data type
+"""
+  5. Write a script that prompts the user to enter side a, side b, and side c of the triangle.
+     Calculate the perimeter of the triangle (perimeter = a + b + c).
+"""
+# a = float(input('Enter side a: '))
+# b = float(input('Enter side b: '))
+# c = float(input('Enter side c: '))
+# perimeter = a + b + c
+# print('The perimeter of the triangle is: {}\n'.format(perimeter))
 
-# Arithmetic operations and assigning the result to a variable
-total = a + b
-diff = a - b
-product = a * b
-division = a / b
-remainder = a % b
-floor_division = a // b
-exponential = a ** b
+"""
+  6. Get length and width of a rectangle using prompt. 
+     Calculate its area (area = length x width) and perimeter (perimeter = 2 x (length + width))
+"""
+# length = float(input('Enter length: '))
+# width = float(input('Enter width: '))
+# area = length * width
+# print('The area of the rectangle is: {}'.format(area))
+# perimeter = 2 * (length + width)
+# print('The perimeter of the rectangle is: {}\n'.format(perimeter))
 
-# I should have used sum instead of total but sum is a built-in function try to avoid overriding builtin functions
-print(total) # if you don't label your print with some string, you never know from where is  the result is coming
-print('a + b = ', total)
-print('a - b = ', diff)
-print('a * b = ', product)
-print('a / b = ', division)
-print('a % b = ', remainder)
-print('a // b = ', floor_division)
-print('a ** b = ', exponential)
+"""
+  7. Get radius of a circle using prompt.
+     Calculate the area (area = pi x r x r) and circumference (c = 2 x pi x r) where pi = 3.14.
+"""
+# radius = float(input('Enter radius: '))
+# area = math.pi * radius * radius
+# print('The area of the circle is: {}'.format(area))
+# circumference = 2 * math.pi * radius
+# print('The circumference of the circle is: {}\n'.format(circumference))
 
-# Declaring values and organizing them together
-num_one = 3
-num_two = 4
+# 8. Calculate the slope, x-intercept and y-intercept of y = 2x - 2
+# x1 = 1
+# x2 = 2
+# y1 = 2 * x1 - 2
+# y2 = 2 * x2 - 2
+# slope1 = (y1 - y2)/(x1 - x2)
+# print('The slope of y = 2x - 2 is {}'.format(slope1))
+# y = 0
+# x = (y + 2)/2
+# print('The x-intercept of y = 2x - 2 is {}'.format((x, y)))
+# x = 0
+# y = 2 * x - 2
+# print('The y-intercept of y = 2x - 2 is {}\n'.format((x, y)))
 
-# Arithmetic operations
-total = num_one + num_two
-diff = num_two - num_one
-product = num_one * num_two
-div = num_two / num_two
-remainder = num_two % num_one
+# 9. Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point (6,10)
+# first = (2, 2)
+# second = (6, 10)
+# slope2 = (first[1] - second[1]) / (first[0] - second[0])
+# print('The slope between point (2, 2) and point (6,10) is {}'.format(slope2))
 
-# Printing values with label
-print('total: ', total)
-print('difference: ', diff)
-print('product: ', product)
-print('division: ', div)
-print('remainder: ', remainder)
+# 10. Compare the slopes in tasks 8 and 9.
+# if slope1 > slope2:
+#     print("The slope of task 8 is greater than the slope of task 9\n")
+# elif slope1 < slope2:
+#     print("The slope of task 8 is less than the slope of task 9\n")
+# else:
+#     print("The slope of task 8 is equal to the slope of task 9\n")
 
 
-# Calculating area of a circle
-radius = 10                                 # radius of a circle
-area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
-print('Area of a circle:', area_of_circle)
 
-# Calculating area of a rectangle
-length = 10
-width = 20
-area_of_rectangle = length * width
-print('Area of rectangle:', area_of_rectangle)
-
-# Calculating a weight of an object
-mass = 75
-gravity = 9.81
-weight = mass * gravity
-print(weight, 'N')
-
-print(3 > 2)     # True, because 3 is greater than 2
-print(3 >= 2)    # True, because 3 is greater than 2
-print(3 < 2)     # False,  because 3 is greater than 2
-print(2 < 3)     # True, because 2 is less than 3
-print(2 <= 3)    # True, because 2 is less than 3
-print(3 == 2)    # False, because 3 is not equal to 2
-print(3 != 2)    # True, because 3 is not equal to 2
-print(len('mango') == len('avocado'))  # False
-print(len('mango') != len('avocado'))  # True
-print(len('mango') < len('avocado'))   # True
-print(len('milk') != len('meat'))      # False
-print(len('milk') == len('meat'))      # True
-print(len('tomato') == len('potato'))  # True
-print(len('python') > len('dragon'))   # False
-
-# Boolean comparison
-print('True == True: ', True == True)
-print('True == False: ', True == False)
-print('False == False:', False == False)
-print('True and True: ', True and True)
-print('True or False:', True or False)
-
-# Another way comparison 
-print('1 is 1', 1 is 1)                   # True - because the data values are the same
-print('1 is not 2', 1 is not 2)           # True - because 1 is not 2
-print('A in Asabeneh', 'A' in 'Asabeneh') # True - A found in the string
-print('B in Asabeneh', 'B' in 'Asabeneh') # False -there is no uppercase B
-print('coding' in 'coding for all') # True - because coding for all has the word coding
-print('a in an:', 'a' in 'an')      # True
-print('4 is 2 ** 2:', 4 is 2 **2)   # True
-
-print(3 > 2 and 4 > 3) # True - because both statements are true
-print(3 > 2 and 4 < 3) # False - because the second statement is false
-print(3 < 2 and 4 < 3) # False - because both statements are false
-print(3 > 2 or 4 > 3)  # True - because both statements are true
-print(3 > 2 or 4 < 3)  # True - because one of the statement is true
-print(3 < 2 or 4 < 3)  # False - because both statements are false
-print(not 3 > 2)     # False - because 3 > 2 is true, then not True gives False
-print(not True)      # False - Negation, the not operator turns true to false
-print(not False)     # True
-print(not not True)  # True
-print(not not False) # False
