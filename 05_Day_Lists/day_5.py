d054dfa28d68be30613799548d7703bd57a132d5
Tuantrung <tuantrung03121997@gmail.com>
2022-01-11 00:21:29 +0700

    Assign task in excercise 5

--- 05_Day_Lists/day_5.py
+++ 05_Day_Lists/day_5.py
@@ -1,179 +1,91 @@
-empty_list = list() # this is an empty list, no item in the list
-print(len(empty_list)) # 0
+# Author: Tuan Trung
+# 30 Days Of Python: Day 5 - Lists
 
-fruits = ['banana', 'orange', 'mango', 'lemon']                     # list of fruits
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot']      # list of vegetables
-animal_products = ['milk', 'meat', 'butter', 'yoghurt']             # list of animal products
-web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] # list of web technologies
-countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+# Exercises - Day 5
 
-# Print the lists and it length
-print('Fruits:', fruits)
-print('Number of fruits:', len(fruits))
-print('Vegetables:', vegetables)
-print('Number of vegetables:', len(vegetables))
-print('Animal products:',animal_products)
-print('Number of animal products:', len(animal_products))
-print('Web technologies:', web_techs)
-print('Number of web technologies:', len(web_techs))
-print('Number of countries:', len(countries))
+# Exercises: Level 1
+# 1. Declare an empty list
 
-# Modifying list
+# 2. Declare a list with more than 5 items
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-first_fruit = fruits[0] # we are accessing the first item using its index
-print(first_fruit)      # banana
-second_fruit = fruits[1]
-print(second_fruit)     # orange
-last_fruit = fruits[3]
-print(last_fruit) # lemon
-# Last index
-last_index = len(fruits) - 1
-last_fruit = fruits[last_index]
+# 3. Find the length of your list
 
-# Accessing itmes
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-last_fruit = fruits[-1]
-second_last = fruits[-2]
-print(last_fruit)       # lemon
-print(second_last)      # mango
+# 4. Get the first item, the middle item and the last item of the list
 
-# Slicing items
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-all_fruits = fruits[0:4] # it returns all the fruits
-# this is also give the same result as the above
-all_fruits = fruits[0:] # if we don't set where to stop it takes all the rest
-orange_and_mango = fruits[1:3] # it does not include the end index
-orange_mango_lemon = fruits[1:]
+# 5. Declare a list called mixed_data_types, put your(name, age, height, marital status, address)
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-all_fruits = fruits[-4:] # it returns all the fruits
-# this is also give the same result as the above
-orange_and_mango = fruits[-3:-1] # it does not include the end index
-orange_mango_lemon = fruits[-3:]
+"""
+  6. Declare a list variable named it_companies and assign initial values Facebook, Google,
+     Microsoft, Apple, IBM, Oracle and Amazon.
+"""
 
+# 7. Print the list using print()
 
-fruits = ['banana', 'orange', 'mango', 'lemon'] 
-fruits[0] = 'Avocado' 
-print(fruits)       #  ['avocado', 'orange', 'mango', 'lemon']
-fruits[1] = 'apple'
-print(fruits)       #  ['avocado', 'apple', 'mango', 'lemon']
-last_index = len(fruits)
-fruits[last_index] = 'lime'
-print(fruits)        #  ['avocado', 'apple', 'mango', 'lime']
+# 8. Print the number of companies in the list
 
-# checking items
-fruits = ['banana', 'orange', 'mango', 'lemon']
-does_exist = 'banana' in fruits
-print(does_exist)  # True
-does_exist = 'lime' in fruits
-print(does_exist)  # False
+# 9. Print the first, middle and last company
 
-# Append
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.append('apple')
-print(fruits)           # ['banana', 'orange', 'mango', 'lemon', 'apple']
-fruits.append('lime')   # ['banana', 'orange', 'mango', 'lemon', 'apple', 'lime]
-print(fruits)
+# 10. Print the list after modifying one of the companies
 
-# insert
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.insert(2, 'apple') # insert apple between orange and mango
-print(fruits)           # ['banana', 'orange', 'apple', 'mango', 'lemon']
-fruits.list(3, 'lime')   # ['banana', 'orange', 'apple', 'mango', 'lime','lemon',]
-print(fruits)
+# 11. Add an IT company to it_companies
 
-# remove
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.remove('banana')
-print(fruits)  # ['orange', 'mango', 'lemon']
-fruits.remove('lemon')
-print(fruits)  # ['orange', 'mango']
+# 12. Insert an IT company in the middle of the companies list
 
-# pop
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.remove()     
-print(fruits)       # ['banana', 'orange', 'mango']
+# 13. Change one of the it_companies names to uppercase (IBM excluded!)
 
-fruits.remove(0)     
-print(fruits)       # ['orange', 'mango'] 
+# 14. Join the it_companies with a string '#;  '
 
-# del 
-fruits = ['banana', 'orange', 'mango', 'lemon']
-del fruits[0]     
-print(fruits)       # ['orange', 'mango', 'lemon']
+# 15. Check if a certain company exists in the it_companies list.
 
-del fruits[1]     
-print(fruits)       # ['orange', 'lemon']
-del fruits
-print(fruits)       # This should give: NameError: name 'fruits' is not defined
+# 16. Sort the list using sort() method
 
-# clear
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.clear()     
-print(fruits)       # []
+# 17. Reverse the list in descending order using reverse() method
 
-# copying a lits
+# 18. Slice out the first 3 companies from the list
 
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits_copy = fruits.copy()     
-print(fruits_copy)       # ['banana', 'orange', 'mango', 'lemon']
+# 19. Slice out the last 3 companies from the list
 
-# join
-positive_numbers = [1, 2, 3,4,5]
-zero = [0]
-negative_numbers = [-5,-4,-3,-2,-1]
-integers = negative_numbers + zero + positive_numbers
-print(integers)
-fruits = ['banana', 'orange', 'mango', 'lemon']
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
-fruits_and_vegetables = fruits + vegetables
-print(fruits_and_vegetables )
+# 20. Slice out the middle IT company or companies from the list
 
-# join with extend
-num1 = [0, 1, 2, 3]
-num2= [4, 5,6]
-num1.extend(num2)
-print('Numbers:', num1)
-negative_numbers = [-5,-4,-3,-2,-1]
-positive_numbers = [1, 2, 3,4,5]
-zero = [0]
+# 21. Remove the first IT company from the list
 
-negative_numbers.extend(zero)
-negative_numbers.extend(positive_numbers)
-print('Integers:', negative_numbers)
-fruits = ['banana', 'orange', 'mango', 'lemon']
-vegetables = ['Tomato', 'Potato', 'Cabbage','Onion', 'Carrot'] 
-fruits.extend(vegetables)
-print('Fruits and vegetables:', fruits )
+# 22. Remove the middle IT company or companies from the list
 
-# count
-fruits = ['banana', 'orange', 'mango', 'lemon']
-print(fruits.count('orange'))   # 1
-ages = [22, 19, 24, 25, 26, 24, 25, 24]
-print(ages.count(24))           # 3
+# 23. Remove the last IT company from the list
 
-# index
-fruits = ['banana', 'orange', 'mango', 'lemon']
-print(fruits.index('orange'))   # 1
-ages = [22, 19, 24, 25, 26, 24, 25, 24]
-print(ages.index(24)) 
-# Reverse
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.reverse()
-print(fruits.reverse())  
-ages = [22, 19, 24, 25, 26, 24, 25, 24]
-ages.reverse()
-print(ages.reverse()) 
+# 24. Remove all IT companies from the list
 
-# sort
-fruits = ['banana', 'orange', 'mango', 'lemon']
-fruits.sort()
-print(fruits) 
-fruits.sort(reverse=True)
-print(fruits)
-ages = [22, 19, 24, 25, 26, 24, 25, 24]
-ages.sort()
-print(ages) 
-ages.sort(reverse=True)
-print(ages) 
+# 25. Destroy the IT companies list
+
+"""
+  26. Join the following lists:
+      front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+      back_end = ['Node','Express', 'MongoDB']
+"""
+
+"""
+  27. After joining the lists in question 26. Copy the joined list and assign it to a variable full_stack.
+      Then insert Python and SQL after Redux.
+"""
+
+# Exercises: Level 2
+"""
+  1. The following is a list of 10 students ages:
+     ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+                 
+     + Sort the list and find the min and max age
+     + Add the min age and the max age again to the list
+     + Find the median age (one middle item or two middle items divided by two)
+     + Find the average age (sum of all items divided by their number )
+     + Find the range of the ages (max minus min)  
+     + Compare the value of (min - average) and (max - average), use abs() method
+"""
+
+# 2. Find the middle country(ies) in the countries list
+
+# 3. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+
+"""
+  4. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark'].
+     Unpack the first three countries and the rest as scandic countries.
+"""
