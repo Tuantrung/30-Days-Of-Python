9fcf6ea5d9b4bb8096652717e23e46c92d9ddb00
Tuantrung <tuantrung03121997@gmail.com>
2022-04-06 23:36:40 +0700

    Refactor source code

--- 21_Day_Classes_and_objects/day_21.py
+++ 21_Day_Classes_and_objects/day_21.py
@@ -13,94 +13,94 @@
 """
 
 
-# class Statistics:
-#     def __init__(self, lst):
-#         self.lst = lst
-#
-#     def count(self):
-#         return len(self.lst)
-#
-#     def sum(self):
-#         return sum(self.lst)
-#
-#     def min(self):
-#         return min(self.lst)
-#
-#     def max(self):
-#         return max(self.lst)
-#
-#     def range(self):
-#         return self.max() - self.min()
-#
-#     def mean(self):
-#         return round(self.sum() / self.count())
-#
-#     def median(self):
-#         self.lst.sort()
-#         if len(self.lst) % 2 != 0:
-#             middle_index = int((len(self.lst) + 1) / 2 - 1)
-#             return self.lst[middle_index]
-#
-#         index1 = int(len(self.lst) / 2) - 1
-#         index2 = index1 + 1
-#         median_age = (self.lst[index1] + self.lst[index2]) / 2
-#         return median_age
-#
-#     def mode(self):
-#         counting_list = list()
-#         for num in set(self.lst):
-#             counting_list.append((num, self.lst.count(num)))
-#         counting_list.sort(key=lambda i: i[1], reverse=True)
-#
-#         max_count = counting_list[0][1]
-#         mode_list = []
-#         for num in set(self.lst):
-#             if self.lst.count(num) == max_count:
-#                 mode_list.append((num, max_count))
-#         return mode_list
-#
-#     def std(self):
-#         mean = self.mean()
-#         total = 0
-#         for num in self.lst:
-#             total += (num - mean) ** 2
-#         variance = total / len(self.lst)
-#         std = variance ** (1 / 2)
-#         return round(std, 1)
-#
-#     def var(self):
-#         mean = self.mean()
-#         total = 0
-#         for num in self.lst:
-#             total += (num - mean) ** 2
-#         variance = total / len(self.lst)
-#         return variance
-#
-#     def freq_dist(self):
-#         counting_list = []
-#         for num in set(self.lst):
-#             counting_list.append((self.lst.count(num) * 100 / len(self.lst), num))
-#         counting_list.sort(reverse=True)
-#         return counting_list
-#
-#     def describe(self):
-#         return f'Count: {self.count()}\n' \
-#                f'Sum: {self.sum()}\n' \
-#                f'Min: {self.min()}\n' \
-#                f'Max: {self.max()}\n' \
-#                f'Range: {self.range()}\n' \
-#                f'Mean: {self.mean()}\n' \
-#                f'Median: {self.median()}\n' \
-#                f'Mode: {self.mode()}\n' \
-#                f'Standard Deviation: {self.std()}\n' \
-#                f'Variance: {data.var()}\n' \
-#                f'Frequency Distribution: {self.freq_dist()}'
-#
-#
-# ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
-#
-# data = Statistics(ages)
-# print(data.describe())
+class Statistics:
+    def __init__(self, lst):
+        self.lst = lst
+
+    def count(self):
+        return len(self.lst)
+
+    def sum(self):
+        return sum(self.lst)
+
+    def min(self):
+        return min(self.lst)
+
+    def max(self):
+        return max(self.lst)
+
+    def range(self):
+        return self.max() - self.min()
+
+    def mean(self):
+        return round(self.sum() / self.count())
+
+    def median(self):
+        self.lst.sort()
+        if len(self.lst) % 2 != 0:
+            middle_index = int((len(self.lst) + 1) / 2 - 1)
+            return self.lst[middle_index]
+
+        index1 = int(len(self.lst) / 2) - 1
+        index2 = index1 + 1
+        median_age = (self.lst[index1] + self.lst[index2]) / 2
+        return median_age
+
+    def mode(self):
+        counting_list = list()
+        for num in set(self.lst):
+            counting_list.append((num, self.lst.count(num)))
+        counting_list.sort(key=lambda i: i[1], reverse=True)
+
+        max_count = counting_list[0][1]
+        mode_list = []
+        for num in set(self.lst):
+            if self.lst.count(num) == max_count:
+                mode_list.append((num, max_count))
+        return mode_list
+
+    def std(self):
+        mean = self.mean()
+        total = 0
+        for num in self.lst:
+            total += (num - mean) ** 2
+        variance = total / len(self.lst)
+        std = variance ** (1 / 2)
+        return round(std, 1)
+
+    def var(self):
+        mean = self.mean()
+        total = 0
+        for num in self.lst:
+            total += (num - mean) ** 2
+        variance = total / len(self.lst)
+        return variance
+
+    def freq_dist(self):
+        counting_list = []
+        for num in set(self.lst):
+            counting_list.append((self.lst.count(num) * 100 / len(self.lst), num))
+        counting_list.sort(reverse=True)
+        return counting_list
+
+    def describe(self):
+        return f'Count: {self.count()}\n' \
+               f'Sum: {self.sum()}\n' \
+               f'Min: {self.min()}\n' \
+               f'Max: {self.max()}\n' \
+               f'Range: {self.range()}\n' \
+               f'Mean: {self.mean()}\n' \
+               f'Median: {self.median()}\n' \
+               f'Mode: {self.mode()}\n' \
+               f'Standard Deviation: {self.std()}\n' \
+               f'Variance: {data.var()}\n' \
+               f'Frequency Distribution: {self.freq_dist()}'
+
+
+ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+data = Statistics(ages)
+print(data.describe())
 
 """
 # 2. Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and 
